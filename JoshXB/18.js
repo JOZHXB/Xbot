29ac1869c626147a1ceac4a3e500414e70840cbc
JOZHXB <84260418+JOZHXB@users.noreply.github.com>
2021-05-19 11:34:26 +0530

    Add files via upload

---
+++ JoshXB/18.js
@@ -0,0 +1,106 @@
+[
+    {
+	"teks": "Name : DilaPye Colmek\nPhoto : -\nVideo : 1\nSize : 90 MB\nDuration : 5 Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1We9TeG1whjz2bmNCrahctK1-Hiy3BtWN/view \n PASSWORD FILE : AA18+#29",
+	"image": "https://i.ibb.co/8jRQ01J/IMG-20201205-223443-917.jpg"
+    },
+    {
+	"teks": "Name : Missluttyv SMA\nPhoto : -\nVideo : 3\nSize : 74 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1TodhXRAHt6MHMqS2-3DYmCQoIFe7yuI-/view \nPASSWORD FILE : AA18+#21",
+	"image": "https://i.ibb.co/KKzmXzY/IMG-20201206-033352-192.jpg"
+    },
+    {
+	"teks": "Name : Arce\nPhoto : 114\nVideo : 16\bSize : 64 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1SwXbmd2sXnYSNYmn_tdCiaBsHHXjYvQM/view \nPASSWORD FILE : AA18+#6",
+	"image": "https://i.ibb.co/8srYLFL/IMG-20201206-034512-690.jpg"
+    },
+    {
+	"teks": "Name : Annisa\nPhoto : 71\nVideo : 2\nSize : 48 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1St_Pb9xNZnrSX_fPfaarno6Bpjc3ArUx/view\nPASSWORD FILE : AA18+#5",
+	"image": "https://i.ibb.co/B32Nq01/E0yui-GCvum-STqdm-G2-6-Os-FBJYMh2-Vd-Da4ayhip-Ub-B4-Xx-Cl-H7-Vt-Ju-j-J-s-Ri-A9kww-Q83-GFPnp-W9nl-Zva.jpg"
+    },
+    {
+    "teks": "Name : 038493040494_0[Ws_Gloia]\nPhoto : 19\nVideo : 1\nSize : 379 MB\nDuration : 8 Minutes\nQuality  : HD++\nRate : ⭐⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1Y0ALFotYLT2F8kKjgxMZ_BFJ6vWKalck/view\nPASSWORD FILE : AA18+#30",
+    "image": "https://i.ibb.co/9psS49T/Lvu-JQ8-Ut-AELo-AGZ0o-F9-RDQNk-W28e-QHj-CZPo-Ak-Wm2u-Rey-RAay-Ku-Ub4-f2-P-m-F6-DLdc67l-Ko-IRy-NZZIth.jpg"
+    },
+    {
+    "teks": "Name : ASD - Ria Bali\nPhoto : -\nVideo : 1\nSize : 165 MB\nDuration : 45 Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1T84UWWj1oJlDAMXd-snZr6gP4Yt61P6P/view\nPASSWORD FILE : AA18+#7",
+    "image": "https://i.ibb.co/BVB8fbj/pv-NJ5-Ja4-VP1fl2-H0b4-Ad-YOzb-HOad-Tzq-JO9-Ms-Ecs-Qz-Sn7s-CMdem-SPv-QBj8qqrt5xk-GEW-o-HKAKs-NRp-GN6.jpg"
+    },
+    {
+    "teks": "Name : Nanad Apr\nPhoto : 18\nVideo : 23\nSize : 357 MB\nDuration : - Minutes\nQuality  : -\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1YO0rvEgbx6Qsrtfm6tTOlvHI7OE5V42p/view\nPASSWORD FILE : AA18+#32",
+    "image": "https://i.ibb.co/BTFg7yT/IEsp-UDTc6-YFw-Hro-E3-Eq-VXGgo-PV8-4-Il4-HWy-Usc-Kw-JMVW4ql-WVBOSzmxjz6-W6r-Dw4-E2-GPo7cygc-HH6-XFy7.jpg"
+    },
+    {
+    "teks": "Name : lily kawai onlyfans\nPhoto : 50\nVideo : 7\nSize : 237 MB\nDuration : - Minutes\nQuality  : HD+\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1TcK_EH1d2vL4bfVhSBm0el_nhmqh2XCS/view\nPASSWORD FILE : AA18+#17",
+    "image": "https://i.ibb.co/bsJfgz7/H3-GSk-Spdcw-Uc-E8t-Mk-A5-ZM6y-MO2-Kk-Fdp5qxr-E0ju-Y3-N-Dx-Ln-YWp-Epx2jug-DEFg-LAloq-1g5-NYr-A2tz-O4.jpg"
+    },
+    {
+    "teks": "Name : Bocil x Tante\nPhoto : -\nVideo : 1\nSize : 156 MB\nDuration : 23 Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1amwgPTf_PLIUc43bxivztFrD0xzdg8Tm/view\nPASSWORD FILE : AA18+#33",
+    "image": "https://i.ibb.co/KNsD0tT/pv-NJ5-Ja4-VP1fl2-H0b4-Ad-YOzb-HOad-Tzq-JO9-Ms-Ecs-Qz-Sn7s-CMdem-SPv-QBj8qqrt5xk-GEW-o-HKAKs-NRp-GN6.jpg"
+    },
+    {
+    "teks": "Name : Badbitch_alter\nPhoto : -\nVideo : 1\nSize : 170 MB\nDuration : 26 Minutes\nQuality  : HD+\nRate : ⭐⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1b2k9RvUeirTzfHBZJkQ9fgw0jJudGP2j/view\nPASSWORD FILE : AA18+#34",
+    "image": "https://i.ibb.co/g6Fwp8S/pv-NJ5-Ja4-VP1fl2-H0b4-Ad-YOzb-HOad-Tzq-JO9-Ms-Ecs-Qz-Sn7s-CMdem-SPv-QBj8qqrt5xk-GEW-o-HKAKs-NRp-GN6.jpg"
+    },
+    {
+    "teks": "Name : Princess nakal\nPhoto : -\nVideo : 1\nSize : 128 MB\nDuration : 15 Minutes\nQuality  : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1bj6nNKADyIzSXONZnmXs7c8E_Ln_3KlM/view\nPASSWORD FILE : AA18+#35",
+    "image": "https://i.ibb.co/gPFMxr6/image.png"
+    },
+    {
+    "teks": "Name : weweci\nPhoto : -\nVideo : 1\nSize : 180 MB\nDuration : 21 Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1cA-pPAB_WRGRRGxhBDk9ouDiynu17xPg/view\nPASSWORD FILE : AA18+#36",
+    "image": "https://i.ibb.co/0YPwyVc/image.png"
+    },
+    {
+    "teks": "Name : Viral Katanya\nPhoto : -\nVideo : 1\nSize : 26 MB\nDuration : 5 Minutes\nQuality  : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : http://www.mediafire.com/file/9o80ka9z61cdtn1/%23Viral_katanya.zip/file",
+    "image": "https://i.ibb.co/vCWZWQP/image.png"
+    },
+    {
+    "teks": "Name : Hijab hitam\nPhoto : -\nVideo : 4\nSize : 20 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1gM7UvH1jzxYek88mQnWBQmou78Gerb_w/view",
+    "image": "https://i.ibb.co/c66fNtR/image.png"
+    },
+    {
+    "teks": "Name : Syifa\nPhoto : 45\nVideo : 25\nSize : 100 MB\nDuration : - Minutes\nQuality  : HD+\nRate : ⭐⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1gTjGotGSlpRCZiLKaZ14sOLjBjirntUb/view\nPassword file : AA18+#27",
+    "image": "https://i.ibb.co/XDB3nxX/image.png"
+    },
+    {
+    "teks": "Name : Danca\nPhoto : 18\nVideo : 13\nSize : 100 MB\nDuration : - Minutes\nQuality  : HD+\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1hDxgMnQ4ps2DkVhFyc51ApdsavGfqs3B/view\nPassword file : AA18+#38",
+    "image": "https://i.ibb.co/s9dJRr1/image.png"
+    },
+    {
+    "teks": "Name : Mira cinggey\nPhoto : 6\nVideo : 10\nSize : 83 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1hcr06URPB2ID6EjzXJAXZuOdmtoZkPs9/view\nPassword file : AA18+#39",
+    "image": "https://i.ibb.co/tqXh4c7/image.png"
+    },
+    {
+    "teks": "Name : Dewi\nPhoto : 266\nVideo : 3\nSize : 150 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1iVfhweyoExOcSCq-LHvJSHRp7pmbNmzg/view\nPassword file : AA18+#40",
+    "image": "https://i.ibb.co/ZBBxgqy/image.png"
+    },
+    {
+    "teks": "Name : Jenn\nPhoto : 50\nVideo : 7\nSize : 105 MB\nDuration : - Minutes\nQuality  : HD+\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1jqYvhwGLp8BgTItHRQpONu1MX3o4o12d/view\nPassword file : AA18+#41",
+    "image": "https://i.ibb.co/jWx7DHR/image.png"
+    },
+    {
+    "teks": "Name : Yanna Davud\nPhoto : 61\nVideo : 5\nSize : 22 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1kIs0lNpZW-R5ub09GiQG6-wgdfagoZsV/view\nPassword file : AA18+#41",
+    "image": "https://i.ibb.co/SRQ590w/image.png"
+    },
+    {
+    "teks": "Name : Nina fika luna\nPhoto : 30\nVideo : 28\nSize : 153 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1kirVFR8V7aYAvY_6s9v5QKyxy5hRQw-X/view\nPassword file : AA18+#42",
+    "image": "https://i.ibb.co/CsL5XWy/image.png"
+    },
+    {
+    "teks": "Name : Ain Penang\nPhoto : 53\nVideo : 7\nSize : 42 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1kzGeQKuKaQKiz6u_JEcPh9wuJGKVB-sv/view\nPassword file : AA18+#43",
+    "image": "https://i.ibb.co/zsSh5t8/image.png"
+    },
+    {
+    "teks": "Name : Jilboobs\nPhoto : -\nVideo : 8\nSize : 84 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1l6M6JXmNiHGaybQiHH39wucVhvLkTjkX/view\nPassword file : AA18+#44",
+    "image": "https://i.ibb.co/L9M2pH4/image.png"
+    },
+    {
+    "teks": "Name : Kiki hijab\nPhoto : -\nVideo : 14\nSize : 190 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1lZDvGBclp35JxhWRpVSCrVOO5YhIQSSn/view\nPassword file : AA18+#45",
+    "image": "https://i.ibb.co/q53mKTp/image.png"
+    },
+    {
+    "teks": "Name : Tifanny\nPhoto : 117\nVideo : 4\nSize : 30 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1lrO9YJ1-MJtHavMpN2kG3nmJSReJtzE6/view\nPassword file : AA18+#46",
+    "image": "https://i.ibb.co/12myv6w/image.png"
+    },
+    {
+    "teks": "Name : Vierannii\nPhoto : 95\nVideo : -\nSize : 12 MB\nDuration : - Minutes\nQuality  : HD\nRate : ⭐⭐⭐⭐\n\nDownload Link\nDownload : https://drive.google.com/file/d/1lrOIZFT1n5XWqkX5uVq7xmmSpKjX70Mf/view\nPassword file : AA18+#47",
+    "image": "https://i.ibb.co/yhbnjtR/image.png"
+    }
+]
